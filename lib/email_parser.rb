ecda5c0e0195306342df053f7237ed028c689667
Jack Quinoa <jquinoa208@gmail.com>
2020-05-31 01:02:09 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailAddressParser
   end
   
   def parse 
-    email_array = email.split(/[, ]).uniq 
+    email_array = email.split(/[, ]/).uniq 
     email_array.reject! {|email| email.empty?}
     email_array
   end
